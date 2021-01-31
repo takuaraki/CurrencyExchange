006e743d914e8d32e6764e747baee8ba85e3c1cc
takuaraki <takuaraki7@gmail.com>
2021-01-31 20:44:29 +0900

    add package views

--- app/src/main/java/com/example/currencyexchange/views/MainActivity.kt
+++ app/src/main/java/com/example/currencyexchange/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.currencyexchange
+package com.example.currencyexchange.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.currencyexchange.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
