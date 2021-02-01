b468d3c887629a5d353e433746e2511ffda4c813
takuaraki <takuaraki7@gmail.com>
2021-02-01 19:29:02 +0900

    fix

--- app/src/main/java/com/example/currencyexchange/models/repository/dao/ExchangeRateDao.kt
+++ app/src/main/java/com/example/currencyexchange/models/repository/dao/ExchangeRateDao.kt
@@ -13,5 +13,5 @@ interface ExchangeRateDao {
     fun getExchangeRates(): Flow<List<ExchangeRate>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertOfUpdate(exchangeRate: ExchangeRate)
+    suspend fun insertOfUpdate(exchangeRate: ExchangeRate)
 }
