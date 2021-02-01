9ac6bf08768fa4c3cc25267ff8788568f3ce4544
takuaraki <takuaraki7@gmail.com>
2021-02-01 19:16:50 +0900

    add logger

--- app/src/main/java/com/example/currencyexchange/models/repository/api/CurrencyAPI.kt
+++ app/src/main/java/com/example/currencyexchange/models/repository/api/CurrencyAPI.kt
@@ -3,6 +3,7 @@ package com.example.currencyexchange.models.repository.api
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -15,7 +16,9 @@ interface CurrencyAPI {
         private const val BASE_URL = "http://api.currencylayer.com"
 
         fun create(): CurrencyAPI {
+            val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
             val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
                 .addInterceptor(object : Interceptor {
                     override fun intercept(chain: Interceptor.Chain): Response {
                         val url = chain.request().url
